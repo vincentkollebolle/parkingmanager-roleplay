e9ab0f045ac631a338deb71d185afc96d9a87711
GaetanccgYnov <gaetan.chollet@ynov.com>
2025-06-12 11:09:10 +0200

    Add primary dashboards

---
+++ frontend/src/components/SimulationCO2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-100 border-l-4 border-gray-400 text-gray-800 p-4 rounded-xl shadow-md">
+    <p class="text-md font-semibold">
+      🌍 Émissions CO₂ pendant l’attente : <strong>{{ co2.totalEmittedWhileWaiting.toFixed(1) }} kg</strong><br>
+      🔥 Moyenne par véhicule rejeté : <strong>{{ co2.averageEmissionPerRejectedVehicle }} kg</strong>
+    </p>
+  </div>
+</template>
+
+<script setup>
+defineProps({ co2: Object })
+</script>
