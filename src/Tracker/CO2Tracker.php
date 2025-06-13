f8a0f0807083d0078a32c8f82d2035c70c1e321b
Benjamin V. <benjamin.vrnt@gmail.com>
2025-06-13 10:17:48 +0200

    fix: namespace

--- src/Tracker/CO2Tracker.php
+++ src/Tracker/CO2Tracker.php
@@ -1,14 +1,22 @@
 <?php
-class CO2Tracker {
+
+namespace App\Tracker;
+
+use App\Vehicle;
+
+class CO2Tracker
+{
     private $carEmission = 0;
     private $allCarEmission = 0;
 
-    public function addRejectedCO2(Vehicle $vehicle) {
+    public function addRejectedCO2(Vehicle $vehicle)
+    {
         // passe par VehicleFactory puis creer vehicule
         $this->carEmission += $vehicle->getCO2();
         $this->allCarEmission++;
     }
-    public function getReport(): array {
+    public function getReport(): array
+    {
         return [
             "totalEmittedWhileWaiting" => round($this->carEmission, 1),
             "averageEmissionPerRejectedVehicle" => $this->allCarEmission ? round($this->carEmission / $this->allCarEmission, 1) : 0
--- src/Tracker/IncomeTracker.php
+++ src/Tracker/IncomeTracker.php
@@ -1,19 +1,26 @@
 <?php
-class IncomeTracker {
+
+namespace App\Tracker;
+
+class IncomeTracker
+{
     private $moneyEarn = 0;
     private $lossOfEarning = 0;
 
-    public function addRevenue($amount) {
+    public function addRevenue($amount)
+    {
         $this->moneyEarn += $amount;
     }
 
-    public function addLost($amount) {
+    public function addLost($amount)
+    {
         $this->lossOfEarning += $amount;
     }
 
     public function onTick($tick) {}
 
-    public function getReport(): array {
+    public function getReport(): array
+    {
         return [
             "moneyEarn" => $this->moneyEarn,
             "lossOfEarning" => $this->lossOfEarning
